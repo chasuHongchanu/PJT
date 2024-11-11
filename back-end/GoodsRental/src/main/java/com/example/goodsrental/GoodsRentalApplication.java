11538828f63b61b9b03a829bba7bea31c0186dc3
Xylitol311 <cktnghd0311@daum.net>
2024-11-11 21:36:42 +0900

    chore : back-end 프로젝트 세팅
    - spring boot 프로젝트 생성
    - .gitignore 파일 추가

---
+++ back-end/GoodsRental/src/main/java/com/example/goodsrental/GoodsRentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodsrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsRentalApplication.class, args);
+    }
+
+}
---
+++ back-end/GoodsRental/src/test/java/com/example/goodsrental/GoodsRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodsrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
