8b3204ee72ec262e7b18d04d68cabbeabd85e003
Xylitol311 <cktnghd0311@daum.net>
2024-11-14 17:05:33 +0900

    refact: repository 제거 및 pr 전 충돌 해결

--- back-end/Trend/src/main/java/com/example/trend/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.trend.user.repository;
-
-import com.example.trend.user.dto.UserSignupRequestDto;
-
-public interface UserRepository {
-    boolean duplicateCheck(String newId);
-
-    void saveUser(UserSignupRequestDto userSignupRequestDto);
-}
--- back-end/Trend/src/main/java/com/example/trend/user/service/UserServiceImpl.java
+++ back-end/Trend/src/main/java/com/example/trend/user/service/UserServiceImpl.java
@@ -3,32 +3,41 @@ package com.example.trend.user.service;
 import com.example.trend.exception.CustomException;
 import com.example.trend.exception.ErrorCode;
 import com.example.trend.user.dto.UserSignupRequestDto;
-import com.example.trend.user.repository.UserRepository;
+import com.example.trend.user.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    UserMapper userMapper;
+
     @Autowired
-    UserRepository userRepository;
+    public UserServiceImpl(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
 
     @Override
     public void signUp(UserSignupRequestDto userSignupRequestDto) {
         // ID 중복 확인
-        boolean isDuplicated = userRepository.duplicateCheck(userSignupRequestDto.getUserId());
+        boolean isDuplicated = duplicateCheck(userSignupRequestDto.getUserId());
         if (isDuplicated) {
             // 에러 반환
             throw new CustomException(ErrorCode.USER_ALREADY_EXISTS);
         }
 
         // 정보 저장
-        userRepository.saveUser(userSignupRequestDto);
-
+        int cnt = userMapper.insertNewUser(userSignupRequestDto);
+        if (cnt != 1) {
+            throw new CustomException(ErrorCode.FAIL_TO_SAVE_USER);
+        }
     }
 
     @Override
     public boolean duplicateCheck(String newId) {
-        boolean result = userRepository.duplicateCheck(newId);
-        return result;
+        int cnt = userMapper.findDuplicatedId(newId);
+        if (cnt > 0) {
+            return true;
+        }
+        return false;
     }
 }
