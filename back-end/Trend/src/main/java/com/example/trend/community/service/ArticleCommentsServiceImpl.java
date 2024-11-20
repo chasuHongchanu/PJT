a46fdca4965ed92e2ad434b08f32ab5d78abd39b
Xylitol311 <cktnghd0311@daum.net>
2024-11-20 20:31:05 +0900

    feat: comment 초기 세팅

---
+++ back-end/Trend/src/main/java/com/example/trend/community/service/ArticleCommentsServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.trend.community.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleCommentsServiceImpl implements ArticleCommentService{
+}
