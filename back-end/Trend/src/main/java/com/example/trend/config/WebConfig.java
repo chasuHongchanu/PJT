b73af26c370686b2bce520d295e1a97d987adb95
Xylitol311 <cktnghd0311@daum.net>
2024-11-24 04:17:16 +0900

    fix: CORS 이슈로 네트워크 에러 발생 현상 해결
    - allowesOrigin 설정 다시 해서 해결됨

--- back-end/Trend/src/main/java/com/example/trend/config/WebConfig.java
+++ back-end/Trend/src/main/java/com/example/trend/config/WebConfig.java
@@ -52,13 +52,13 @@ public class WebConfig implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-                        .allowedOrigins("http://localhost:5173") // 허용할 Origin
+                registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+                        .allowedOrigins("http://localhost:5174", "http://localhost:5173") // 허용할 Origin
+//                        .allowedOrigins("http://localhost:5173") // 허용할 Origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                        .allowedHeaders("*") // 허용할 헤더
-                        .allowCredentials(true)
-                        .exposedHeaders("Authorization"); // Authorization 헤더 노출 // 인증 정보 포함 허용 (쿠키 등)
-
+                        .allowedHeaders("*") // 허용할 요청 헤더
+                        .allowCredentials(true) // 인증 정보 포함 허용 (쿠키 등)
+                        .exposedHeaders("Authorization"); // 노출할 응답 헤더
             }
         };
     }
